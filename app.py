505e7bae7554e9251ca5e68899f9d39a84aa1031
Balázs Ádám Sidlóczki <sidbalazs18@gmail.com>
2023-09-17 12:20:18 +0200

    Implement download function

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import InputRequired
+import requests
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "SECRET_KEY"
@@ -11,10 +12,18 @@ class ImageForm(FlaskForm):
     file_name = StringField(validators=[InputRequired()], render_kw={"placeholder": "FILE_NAME"})
     submit = SubmitField("Download")
 
+def download_image(url, file_name):
+    r = requests.get(url)
+    with open(file_name + '.jpg', 'wb') as f:
+        f.write(r.content)
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = ImageForm()
+    if form.validate_on_submit():
+        download_image(form.url.data, form.file_name.data)
     return render_template('index.html', form=form)
 
 
