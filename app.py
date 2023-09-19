e13aa05143999cb190d3fa740b1da50db8a028d1
Balázs Ádám Sidlóczki <sidbalazs18@gmail.com>
2023-09-19 19:01:48 +0200

    minor changes

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, request, flash, redirect, url_for, send_file
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import InputRequired
 import requests
-#from pathfinder import get_folder_path, get_os
+from pathfinder import get_folder_path, get_os
 import re
-from io import BytesIO
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "SECRET_KEY"
@@ -24,7 +23,7 @@ def valid_url(url):
     return re.match(pattern, url)
 
 
-""" def download_image(url, file_name):
+def download_image(url, file_name):
     r = requests.get(url)
     operating_system = get_os()
     if operating_system == "Linux":
@@ -32,7 +31,7 @@ def valid_url(url):
     else:
         path = get_folder_path() + '\\' + file_name + '.jpg'
     with open(path, 'wb') as f:
-        f.write(r.content) """
+        f.write(r.content)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -42,24 +41,13 @@ def index():
     if request.method == 'POST':
         if form.validate_on_submit():
             if valid_url(form.url.data):
-                #download_image(form.url.data, form.file_name.data)
+                download_image(form.url.data, form.file_name.data)
                 flash("The image has been downloaded!")
             else:
                 flash("The URL you provided is wrong!")
         return redirect(url_for('index'))
 
     return render_template('index.html', form=form)
-
-@app.route('/download')
-def download_file():
-    # Távoli URL, ahonnan a fájlt letöltjük
-    remote_url = 'https://www.thehappycatsite.com/wp-content/uploads/2021/01/lynx-point-siamese-HC-long.jpg'
-
-    # Letöltjük a távoli fájlt a BytesIO objektumba
-    response = requests.get(remote_url)
-    if response.status_code == 200:
-        file_data = BytesIO(response.content)
-        return send_file(file_data, as_attachment=True, download_name='lynx-point-siamese-HC-long.jpg')
 
 
 if __name__ == '__main__':
